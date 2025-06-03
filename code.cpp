d3e8c01677ab9aa779dfcea7d4063923339d06f1
StarLord824 <shuklaabhinav824@gmail.com>
2025-06-03 15:00:24 +0530

    set up for initian cp templates done in cpp and java

---
+++ code.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+//macros
+#define ll long long
+#define endl "\n"
+#define pb push_back
+#define mp make_pair
+#define pii pair<int, int>
+#define vi vector<int>
+
+//constants
+#define INF 1000000000
+#define MOD 1000000007
+#define EPS 1e-9
+#define PI 3.14159265358979323846
+#define MAX 100005
+#define MIN -100005
+
+//main function
+int main() {
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+   #endif
+
+   cout << INT16_MAX<< " " << INT32_MAX << " " << INT64_MAX << endl;
+   cout << INT16_MIN << " " << INT32_MIN << " " << INT64_MIN << endl;
+   cout << UINT16_MAX << " " << UINT32_MAX << " " << UINT64_MAX << endl;
+   return 0;
+}
